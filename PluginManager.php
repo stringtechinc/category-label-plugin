7f254127f5da68ba759d6e818d4d8a05b4dbe305
linxy49 <rin@busicall.co.jp>
2018-05-23 12:07:38 +0900

    add cache clear

---
+++ PluginManager.php
@@ -0,0 +1,83 @@
+<?php
+
+/*
+ * This file is part of the FeaturedProduct
+ *
+ * Copyright (C) 2018 StringTech Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\CategoryLabel;
+
+use Eccube\Application;
+use Eccube\Plugin\AbstractPluginManager;
+use Eccube\Util\Cache;
+
+class PluginManager extends AbstractPluginManager
+{
+
+    public function __construct()
+    {
+    }
+
+    /**
+     * プラグインインストール時の処理
+     *
+     * @param $config
+     * @param Application $app
+     * @throws \Exception
+     */
+    public function install($config, Application $app)
+    {
+        Cache::clear($app, false);
+    }
+
+    /**
+     * プラグイン削除時の処理
+     *
+     * @param $config
+     * @param Application $app
+     */
+    public function uninstall($config, Application $app)
+    {
+        Cache::clear($app, false);
+    }
+
+    /**
+     * プラグイン有効時の処理
+     *
+     * @param $config
+     * @param Application $app
+     * @throws \Exception
+     */
+    public function enable($config, Application $app)
+    {
+        Cache::clear($app, false);
+    }
+
+    /**
+     * プラグイン無効時の処理
+     *
+     * @param $config
+     * @param Application $app
+     * @throws \Exception
+     */
+    public function disable($config, Application $app)
+    {
+        Cache::clear($app, false);
+    }
+
+    /**
+     * プラグイン更新時の処理
+     *
+     * @param $config
+     * @param Application $app
+     * @throws \Exception
+     */
+    public function update($config, Application $app)
+    {
+        Cache::clear($app, false);
+    }
+}
